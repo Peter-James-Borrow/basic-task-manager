266c80778ef8afc2059aa93ee844d0f41c16e891
Peter-James-Borrow <borrow.peter83@gmail.com>
2021-02-06 18:15:13 +0000

    removed all but one input field and buttons, will add them back on page with button click. javascripy: added function for delete button.

--- todo-basic.js
+++ todo-basic.js
@@ -1,26 +1,46 @@
-const addTask = document.querySelectorAll('.add');
-const inputField = document.querySelector('text');
-const deleteTask = document.querySelectorAll('.delete');
+const addTask = document.querySelector('.add');
+const deleteTask = document.querySelector('.delete');
+const inputField = document.querySelector('.text');
 const list = document.querySelector('.container');
 
 
 
-function addToList(e) {
+function addToList(e, paragraph) {
     e.preventDefault();
-    
-
-    console.log('HELLO');
+    paragraph = document.createElement('p');
+    paragraph.innerHTML = inputField.value;
+    paragraph.classList.add('special');
+    list.appendChild(paragraph);
 }
-
 addTask.forEach(function(additem) {
     additem.addEventListener('click', addToList);
-});
-
-function deleteItem(e) {
-    e.preventDefault();
+    
+    
+    function deleteItem(removeItem) {
+    removeItem = document.querySelector('p');
+    removeItem.classList.remove('special');
+    removeItem.remove();
     console.log('got clicked');
 }
-
+   
 deleteTask.forEach(button => {
     button.addEventListener('click', deleteItem);
 })
+});
+
+
+
+    
+    
+    
+    
+   
+    
+
+    
+    
+   
+
+    
+
+
